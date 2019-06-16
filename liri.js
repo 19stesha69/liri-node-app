934ef99eed6302dac3d21c932281526f1d635c53
Your Name <you@example.com>
2019-06-16 12:27:02 -0700

    added keys to .env file

---
+++ liri.js
@@ -0,0 +1,5 @@
+//This code reads and sets any environment variables with the dotenv package
+require("dotenv").config();
+
+//this code is required to import the keys.js file and store it in a variable
+var keys = require("./keys.js");
